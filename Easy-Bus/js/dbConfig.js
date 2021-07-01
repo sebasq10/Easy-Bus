7f6e04b0505664f1edd23ba7712fe59d87e1e5bf
Kenneth Gomez <30990169+KetoGomez@users.noreply.github.com>
2021-06-30 20:27:59 -0700

    Intento de crear conexion a base de datos
    Intento de crear conexion a base de datos, el cual fallo

---
+++ Easy-Bus/js/dbConfig.js
@@ -0,0 +1,13 @@
+const config= {
+    user:'admin',
+    password: '',
+    server: 'DESKTOP-FDUQL33',
+    database: 'Easy-bus',
+    options: {
+        trusted: true,
+        enableArithAort: true,
+    },
+    port: 1433
+}
+
+module.exports = config;klj
---
+++ Easy-Bus/js/server.js
@@ -0,0 +1,39 @@
+// const rest = new (require('rest-mssql-nodejs'))({
+//     user: 'admin',
+//     password: '1T2Bm4k3r',
+//     server: 'DESKTOP-FDUQL33\\MSSQLSERVER',
+//     database: 'Easy-bus'
+// });
+
+var sql = require('mssql');
+
+var dbConfig= {
+    server:"localhost",
+    database: "Easy-Bus",
+    user: "admin",
+    password: "",
+    port: 8080
+}
+
+async function getTable(){
+    var conn= await new sql.connect(dbConfig);
+    var req= await new sql.request(conn);
+
+    conn.connect(function(err){
+        if(err){
+            console.log(err);
+            return;
+        }
+
+
+        req.query("SELECT * FROM rol", function(err, recordset){
+            if(err){
+                console.log(err);
+                return;
+            }else{
+                console.log(recordset);
+            }
+            conn.close();
+        });
+    });
+}
