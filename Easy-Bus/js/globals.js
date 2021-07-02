3c4ab36a24d4a5db113fc101e88e7f1636274baa
KetoGomez <30990169+KetoGomez@users.noreply.github.com>
2021-07-01 22:55:21 -0600

    inicio agregar usuarios

---
+++ Easy-Bus/js/globals.js
@@ -0,0 +1,18 @@
+(function () {
+
+    
+    let usuario = {
+        usuarioID: 0,
+        usuario: "",
+        contrasenia: "",
+        nombre: "",
+        apellido1: "",
+        apellido2: "",
+        fechaNacimiento: "",
+        rolID: 0,
+        metodoPagoID: "",
+        activo: "",
+    }
+
+    let listaUsuarios=[];
+})();
